0ed2fc33ac866975ddab5c54c28ad857de4af473
e <eduardof.miotto@gmail.com>
2024-12-08 19:10:33 -0300

    login autenticação

--- PROG_II/integrador/src/TelaLogin.js
+++ PROG_II/integrador/src/TelaLogin.js
@@ -1,25 +1,59 @@
 import { Box, IconButton, InputLabel, FormControl, InputAdornment, Container, Paper, OutlinedInput, Button, AppBar, Toolbar, Typography } from "@mui/material";
 
 import * as React from 'react';
+import axios from 'axios';
 
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 
 
-function TelaLogin() {
+function TelaLogin(props) {
     
     const [showPassword, setShowPassword] = React.useState(false);
-      
     const handleClickShowPassword = () => setShowPassword((show) => !show);
-      
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
-      
     const handleMouseUpPassword = (event) => {
       event.preventDefault();
     }
+
+    const [username, setUsername] = React.useState("");
+	const [passwd, setPasswd] = React.useState("");
+
+	const [openMessage, setOpenMessage] = React.useState(false);
+	const [messageText, setMessageText] = React.useState("");
+	const [messageSeverity, setMessageSeverity] = React.useState("success");
+    
+    async function enviaLogin(event) {
+        console.log('evia login');
+		event.preventDefault();
+		try {
+			console.log(props.user);
+			const response = await axios.post("/login", {
+				username: username,
+				password: passwd,
+			});
+			if (response.status >= 200 && response.status < 300) {
+				// Salva o token JWT na sessão
+				localStorage.setItem("token", response.data.token);
+				// seta o estado do login caso tudo deu certo
+				props.handleLogin(true);
+				console.log(props.user);
+			} else {
+				// falha
+				console.error("Falha na autenticação");
+			}
+		} catch (error) {
+			console.log(error);
+			setOpenMessage(true);
+			setMessageText("Falha ao logar usuário!");
+			setMessageSeverity("error");
+		}
+	}
+
+
 
     return (
         <div style={{ height: '80vh', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', paddingRight: '33%' }}>
@@ -78,7 +112,7 @@ function TelaLogin() {
                                 />
                         </FormControl>
 
-                        <Button variant="contained" sx={{ backgroundColor: 'black', mt: 3 }}>Login</Button>
+                        <Button variant="contained" sx={{ backgroundColor: 'black', mt: 3 }} onClick={enviaLogin}>Login</Button>
 
                     </Box>
 
